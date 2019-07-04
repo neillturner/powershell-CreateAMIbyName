4b5006c506c837dd04948d172faa713cac9ccd75
neill.turner <neill.turner@hindawi.com>
2019-07-04 16:31:11 +0100

    sysprep instance befor create ami

--- README.md
+++ README.md
@@ -2,3 +2,38 @@
 Powershell Create AMI by Name 
 
 from https://www.yobyot.com/aws/how-to-tag-amis-and-snapshots-with-aws-powershell-cmdlets/2014/10/21/
+
+Takes an instance tag and creates an AWS snapshot and AMI.
+
+To just take an AMI of the root disk (i.e. C drive) use CreateRootAMIbyName.ps1
+
+To sysprep the server so EC2 administrator password can be obtained from the AWS console
+* make sure sysprep-ec2config.ps1 is on the server in the C:/Users/Administrator folder.
+* run first the RunSysprep.ps1 script before the CreateAMIbyName.ps1 or CreateRootAMIbyName.ps1 script.
+* RunSysprep.ps1 accepts an optional parameter where the script name can be specified if differently named.
+
+This can be specified in a Jenkins pipeline via:
+
+```
+pipeline {
+     agent any
+     parameters {
+        choice(choices: ['servera','serverb'],
+        description: 'What EC2 Instance?', name: 'instanceNameTag')
+        string(defaultValue: "backup", description: 'What comment?', name: 'note')
+     }
+     stages {
+         stage ('CreateAMIByName') {
+             steps {
+                 script {
+                     git branch: 'master',
+                        url: 'https://github.com/neillturner/powershell-CreateAMIbyName.git'
+                     powershell returnStatus: true, script: ".\\RunSysprep.ps1 -instanceNameTag ${params.instanceNameTag}
+                     powershell returnStatus: true, script: ".\\CreateRootAMIbyName.ps1 -instanceNameTag ${params.instanceNameTag}-note ${params.note} -architecture i386 "
+
+                 }
+             }
+         }
+     }
+}
+```
---
+++ RunSysprep.ps1
@@ -0,0 +1,61 @@
+<#
+Run remote powershell script to sysprep a windows server. 
+
+#>
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string[]]$instanceNameTag,
+    [Parameter(Mandatory = $false)]
+    [string]$scriptName
+)
+Import-Module AWSPowerShell
+if ($scriptName -eq "") {
+   $scriptName = 'C:\Users\Administrator\sysprep-ec2config.ps1'
+ }
+
+$array = @($instanceNameTag)
+
+foreach ($nameTag in $array) 
+{
+    $i = (Get-EC2Instance -Filter @{name ='tag:Name'; values = $nameTag}).instances # Create array of type Amazon.EC2.Model.Instance
+    foreach ($instance in $i) # In case there are duplicated name tags, offer a choice to create an AMI only for the instances in running or stopped state
+    {
+        switch ($instance.state.code)
+        {
+
+            0 {
+                # Status is pending
+                $instance.instanceID + " status is " + $instance.state.code + ": pending, skipped"
+            }
+            { ($_ -eq 16) } {
+                # Status is running
+                Write-Host "`nSysprep the instance for $nameTag from instance $($instance.InstanceID), status = $($instance.state.name)?" -ForegroundColor Yellow
+                $cmd = Send-SSMCommand -InstanceId $instance.InstanceID -DocumentName AWS-RunPowerShellScript -Comment 'sysprep the instance' -Parameter @{'commands'=@($scriptName)}
+                Start-Sleep -Seconds 15 # Wait a few seconds to let it complete
+                $result = Get-SSMCommand -CommandId $runPSCommand.CommandId
+                Write-Host "`nCommand Status $($result.status[0])" -ForegroundColor Yellow
+            }
+            32 {
+                # Status is shutting-down
+                $instance.instanceID + " status is " + $instance.state.code + ": shutting down,skipped"
+            }
+            48 {
+                # Status is terminated
+                $instance.instanceID + " status is " + $instance.state.code + ": terminated, skipped"
+            }
+            64 {
+                # Status is stopping
+                $instance.instanceID + " status is " + $instance.state.code + ": stopping, skipped"
+            }
+            80 {
+                # Status is stopped
+                $instance.instanceID + " status is " + $instance.state.code + ": stopped, skipped"
+            }
+            default
+            {
+                Write-Error "No valid states detected for any of the instances associated with the specified name tag."
+            }
+        }
+    }
+}
